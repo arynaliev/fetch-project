9867ce211a2d4a4b16ce6fed6f668cec779ddbe8
Azim <azimarynaliev@icloud.com>
2023-12-19 19:16:01 -0600

    updated

--- script.js
+++ script.js
@@ -2,21 +2,47 @@
 
 const getImgBtn = document.querySelector(".getImgBtn");
 
-const onClickHandler = () => {
+const onClickHandler = async () => {
   getImgBtn.textContent = "Loading..";
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then((res) => res.json())
-    .then((data) => {
-      const dogImg = document.createElement("img");
-      dogImg.className = "dogImg";
-      dogImg.src = data.message;
-      dogImg.alt = "Dog";
-      dogImg.style.width = "200px";
 
-      const imgBox = document.querySelector(".imgBox");
-      console.log(data);
-      getImgBtn.textContent = "Get Image";
-      imgBox.append(dogImg);
-    })
-    .catch((err) => console.log("error:", err));
+  try {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+
+    const dogImg = document.createElement("img");
+    dogImg.className = "dogImg";
+    dogImg.src = data.message;
+    dogImg.alt = "Dog";
+
+    const imgBox = document.querySelector(".imgBox");
+    console.log(data);
+    getImgBtn.textContent = "Get Image";
+    imgBox.append(dogImg);
+  } catch (err) {
+    console.log("error:", err);
+  }
 };
+
+/*
+const audio = document.querySelector(".audio");
+
+const onChangeHandler = (e) => {
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${e.target.value}`)
+    .then((res) => res.json())
+    .then((data) => data);
+  audio.src = data[0].phonetics[0].audio;
+  // audio.src =
+  //   "https://api.dictionaryapi.dev/media/pronunciations/en/ball-us.mp3";
+  audio.play();
+};*/
+
+// const getDogImages = async () => {
+//   try {
+//     const res = await fetch("https://dog.ceo/api/breeds/image/random");
+//     const data = await res.json();
+//   } catch (err) {
+//     console.log("error:", err);
+//   }
+// };
+
+// getDogImages();
