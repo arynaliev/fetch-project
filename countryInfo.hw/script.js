038630a18cff1484c8a0d9d0324206903b26e986
Azim <azimarynaliev@icloud.com>
2023-12-21 14:20:05 -0600

    updated

--- countryInfo.hw/script.js
+++ countryInfo.hw/script.js
@@ -77,18 +77,49 @@ const fetchCountry = async (countryName) => {
 };
 
 const onSearchClick = async () => {
-  const data = await fetchCountry(countryName);
-  console.log(data);
+  // fetch
+  try {
+    const data = await fetchCountry(countryName);
+    console.log(data);
 
-  // select dom elements
-  const name = document.querySelector("#name");
-  const population = document.querySelector("#population");
-  const currency = document.querySelector("#currency");
-  const flag = document.getElementById("flag");
-  //
-  flag.src = data[0].flags.png;
-  name.textContent = data[0].name.official;
-  population.textContent = data[0].population;
-  let currencyKey = Object.keys(data[0].currencies);
-  currency.textContent = data[0].currencies[currencyKey[0]].name;
+    // create html elements
+    const container = document.querySelector(".container");
+    const countryInfo = document.createElement("div");
+    countryInfo.className = "countryInfo";
+    const flag = document.createElement("img");
+    const name = document.createElement("h3");
+    const population = document.createElement("p");
+    const currency = document.createElement("p");
+
+    //giving attributes
+    flag.alt = "Flag";
+    flag.style.width = "100px";
+    flag.src = data[0].flags.png;
+    name.textContent = data[0].name.official;
+    population.textContent = `Population is about ${data[0].population}`;
+    let currencyKey = Object.keys(data[0].currencies);
+    currency.textContent = `Currency is ${
+      data[0].currencies[currencyKey[0]].name
+    }`;
+
+    // appending
+    countryInfo.append(flag, name, population, currency);
+    container.append(countryInfo);
+    document.body.append(container);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // clearing input after search click
+  const searchInput = document.getElementById("searchInput");
+  searchInput.value = "";
 };
+
+localStorage.setItem("onSearchClick", onSearchClick.toString());
+
+const storedClickFunction = localStorage.getItem("onSearchClick");
+if (storedClickFunction) {
+  const onSearchClickRestored = new Function("return " + storedClickFunction)();
+  const searchBtn = document.querySelector("#searchBtn");
+  searchBtn.addEventListener("click", onSearchClickRestored);
+}
